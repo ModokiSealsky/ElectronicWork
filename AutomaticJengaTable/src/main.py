8a1e8162e4db88096aa8540a02ade2f4511ce54c
yasutoshikitahara <yasutoshi.kitahara@gmail.com>
2024-11-14 21:47:49 +0900

    実行処理をmain.pyに変更

--- AutomaticJengaTable/src/main.py
+++ AutomaticJengaTable/src/main.py
@@ -2,18 +2,21 @@ from machine import PWM, Pin
 import utime
 from pico.Led import Led
 from pico.Servo import Servo
-from pico.StepMoter import StepMoter
+from pico.StepMoter28BYJ48 import StepMoter28BYJ48
+from pico.StepMoterB import StepMoterB
 from pico.CoreInfo import CoreInfo
 from jengatable.JengaTableController import JengaTableController
 from jengatable.JengaTable import JengaTable
 
 # GPIO定義
-P_MODE_SW = 16               # モード切替スイッチ（GPIO.IN, PULL_DOWN）
-P_READ_SW = 17               # リロード起動スイッチ（GPIO.IN, PULL_DOWN）
+P_MODE_SW = 17               # モード切替スイッチ（GPIO.IN, PULL_DOWN）
+P_READ_SW = 18               # リロード起動スイッチ（GPIO.IN, PULL_DOWN）
 P_STEP_MT = [10, 11, 12, 13] # エレベーター用ステップモーター（I2C）
+P_STEP_MT_B = [10, 11, 12] # エレベーター用ステップモーター（I2C）
 P_TURN_SV = 1                # ターンテーブル用サーボ（I2C）
 P_PUSH_SV = 0                # 押し出し機用サーボ（I2C）
-P_EXEC_LT = 18               # 動作中LED（GPIO。OUT）
+P_EXEC_LT = 16               # 動作中LED（GPIO。OUT）
+STEPMOTOTER_IS_B = False     # バイボーラ式ステップモーター利用
 
 # スイッチセットアップ
 reload_switch_counter = 0
@@ -24,32 +27,37 @@ mode_switch = Pin(P_MODE_SW, Pin.IN, Pin.PULL_DOWN)
 jenga_table = JengaTable()
 jenga_table.setPushServo(Servo(P_PUSH_SV))
 jenga_table.setTurnTableServo(Servo(P_TURN_SV))
-jenga_table.setElevetorStepMotor(StepMoter(P_STEP_MT[0], P_STEP_MT[1], P_STEP_MT[2], P_STEP_MT[3]))
+if STEPMOTOTER_IS_B:
+    jenga_table.setElevetorStepMotor(StepMoterB(P_STEP_MT_B[0], P_STEP_MT_B[1], P_STEP_MT_B[2]))
+else:
+    jenga_table.setElevetorStepMotor(StepMoter28BYJ48(P_STEP_MT[0], P_STEP_MT[1], P_STEP_MT[2], P_STEP_MT[3]))
 jenga_table.setExecLed(Led(P_EXEC_LT))
+# ジェンガテーブル微調整
+jenga_table.setTurnTableServoRange(3, 153)
 
 # コントローラーセットアップ
 table_controller = JengaTableController(2, 1, 0)
 coreInfo = CoreInfo()
 
 print("セットアップ完了")
-
+jenga_table.reload()
 while True:
 # メインループ:start
     if mode_switch.value() == 1:
         # マニュアルモードスイッチON
         v_elv = table_controller.getVolumeElevetor()
         if v_elv < 50:
-            v_elv = -10
+            v_elv = -100
         elif v_elv > 130:
-            v_elv = 10
+            v_elv = 100
         else:
             v_elv = 0
         v_turn = table_controller.getVolumeTurntable()
         v_push = table_controller.getVolumePusher()
         print("マニュアルモード|E:{0}, T:{1}, P:{2}".format(str(v_elv), str(v_turn), str(v_push)))
         jenga_table.elevatorMove(v_elv)
-        jenga_table.tableTurn(v_turn)
-        jenga_table.pushMove(v_push)
+        jenga_table.tableMove(int(v_turn))
+        jenga_table.pushMove(int(v_push))
     else:
         # マニュアルモードスイッチOFF
         print("待機モード|S:{0}".format(reload_switch_counter))
