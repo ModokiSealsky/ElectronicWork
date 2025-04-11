0e28d0b05059ff03eaf8b80ddffca11530d930fc
yasutoshikitahara <yasutoshi.kitahara@gmail.com>
2025-04-11 16:29:14 +0900

    サーボの角度指定に小数点以下を指定可能に変更

--- AutomaticJengaTable/src/pico/Servo.py
+++ AutomaticJengaTable/src/pico/Servo.py
@@ -7,22 +7,29 @@ class Servo:
     """
     SV_MAX_DUTY = 65025
     SV_FREQ = 50
-    def __init__(self, pin_no):
+    DUTY_RATE = 9.5 / 1800.0
+
+    def __init__(self, pin_no:int):
         """コンストラクタ
         Args:
-            pin_no (int): 制御用PWMピン番号
+            pin_no: 制御用PWMピン番号
         """
         self.s = PWM(Pin(pin_no))
         self.s.freq(self.SV_FREQ)
-    def turn(self, angle):
+
+    def turn(self, angle:float):
         """角度変更
         Args:
-            angle (int): 0～180の角度値
+            angle: 0.0～180.0の角度値
         """
-        if angle < 0:
-            angle = 0
-        if angle > 180:
-            angle = 180
+        if angle < 0.0:
+            angle = 0.0
+        if angle > 180.0:
+            ag = 180.0
         duty = int((angle * 9.5 / 180 + 2.5) * 65535 / 100)
+        print(duty)
+        duty = int((angle * 9.5 / 180 + 2.5) * 655.35)
+        print(duty)
+        duty = int((angle * 9.5 / 180.0 + 2.5) * 655.35)
+        print(duty)
         self.s.duty_u16(duty)
-        
