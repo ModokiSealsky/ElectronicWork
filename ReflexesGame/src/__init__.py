3089732cd871d9f23453fe2958a5c2090e0a7f44
yasutoshikitahara <yasutoshi.kitahara@gmail.com>
2025-06-11 00:35:47 +0900

    メインファイルをライブラリクラス使用に変更

--- ReflexesGame/src/__init__.py
+++ ReflexesGame/src/__init__.py
@@ -1,3 +1,5 @@
 from .Buzzer import Buzzer
 from .ReflexesGame import ReflexesGame
 from .ScoreBord import ScoreBord
+from .Led import Led
+from .InputSwitch import InputSwitch
--- ReflexesGame/src/main.py
+++ ReflexesGame/src/main.py
@@ -1,47 +1,45 @@
 """反応速度を測るゲーム_メイン(待機)ループ"""
-from machine import Pin
 import utime
-from .__init__ import Buzzer ,ReflexesGame ,ScoreBord
+from .__init__ import Buzzer, InputSwitch, Led ,ReflexesGame ,ScoreBord
 
-# ピン指定 ------------------------------------------------
-P_BUZZER_L  = 0
-P_BUZZER_H  = 0
-P_LIGHTS    = [0,1,2,3,4,5,6,7]
-P_BUTTONS   = [0,1,2,3,4,5,6,7]
-P_START_BTN = 0
-P_HIGHSCORE = 0
-P_GAMEOVER  = 0
+# ピン指定 ---------------------------------------------------------------------
+P_BUZZER_L  = 17
+P_BUZZER_H  = 16
+P_LIGHTS    = [2 ,4 ,6, 8, 10, 12]
+P_BUTTONS   = [3, 5, 7, 9, 11, 13]
+P_START_BTN = 19
+P_HIGHSCORE = 14
+P_GAMEOVER  = 15
 CH_7SEG_DP  = 0
-P_7SEG_SDA  = 1
-P_7SEG_SCL  = 2
-# ---------------------------------------------------------
+ADD_7SEG_D  = 0x70
+P_7SEG_SDA  = 20
+P_7SEG_SCL  = 21
+P_7SEG_ENG  = 22
+# ------------------------------------------------------------------------------
 
-# セットアップ ---------------------------------------------
-highe_score = 9999
-buzzer_l = Buzzer(P_BUZZER_L)
-buzzer_h = Buzzer(P_BUZZER_H)
-start_btn = Pin(P_START_BTN, Pin.IN)
-light_highscore = Pin(P_HIGHSCORE, Pin.OUT)
-light_gameover = Pin(P_GAMEOVER, Pin.OUT)
+# セットアップ -----------------------------------------------------------------
+start_btn = InputSwitch(P_START_BTN)
 score_bord = ScoreBord(CH_7SEG_DP, P_7SEG_SCL, P_7SEG_SDA)
-game_logic = ReflexesGame(P_LIGHTS, P_BUTTONS
-                          , buzzer_l, buzzer_h
-                          , score_bord)
-# ---------------------------------------------------------
+score_bord.setEngPin(P_7SEG_ENG)
+score_bord.setI2cAddr(ADD_7SEG_D)
+game_logic = ReflexesGame([Led(pin_no) for pin_no in P_LIGHTS]
+                          , [InputSwitch(pin_no) for pin_no in P_BUTTONS]
+                          , Buzzer(P_BUZZER_L)
+                          , Buzzer(P_BUZZER_H)
+                          , score_bord
+                          , Led(P_HIGHSCORE)
+                          , Led(P_GAMEOVER))
+# ------------------------------------------------------------------------------
 
-# 待機ループ -----------------------------------------------
+# 待機ループ -------------------------------------------------------------------
 start_btn_cnt = 0
 while True:
-    if start_btn.value() == 1:
+    if start_btn.isOn():
         start_btn_cnt += 1
     else:
         start_btn_cnt = 0
     if start_btn_cnt > 2:
         print("ゲームスタート")
-        light_highscore.off()
-        light_gameover.off()
         game_logic.gameStert()
-        if game_logic.isScoreUpdated():
-            light_highscore.on()
     utime.sleep_ms(1000)
-# ---------------------------------------------------------
+# ------------------------------------------------------------------------------
