e9289c51c3a53252c91fcc6a872906c4cfed4d03
yasutoshikitahara <yasutoshi.kitahara@gmail.com>
2025-06-18 19:19:02 +0900

    難易度変更を追加

--- ReflexesGame/src/main.py
+++ ReflexesGame/src/main.py
@@ -8,6 +8,7 @@ P_BUZZER_L  = 17
 P_BUZZER_H  = 16
 P_LIGHTS    = [2 ,4 ,6, 8, 10, 12]
 P_BUTTONS   = [3, 5, 7, 9, 11, 13]
+P_MODE_SEL  = 18
 P_START_BTN = 19
 P_HIGHSCORE = 14
 P_GAMEOVER  = 15
@@ -19,6 +20,16 @@ P_7SEG_SCL  = 21
 # ------------------------------------------------------------------------------
 
 # セットアップ -----------------------------------------------------------------
+# 難易度取得
+mode_selector = InputSwitch(P_MODE_SEL)
+order_size: int
+if mode_selector.is_off():
+    print("--Mode:Normal--")
+    order_size = 10
+else:
+    order_size = 20
+    print("--Mode:Hard--")
+# ゲーム初期化
 lights = [Led(pin_no) for pin_no in P_LIGHTS]
 start_btn = InputSwitch(P_START_BTN)
 score_bord = ScoreBord(CH_7SEG_DP, P_7SEG_SCL, P_7SEG_SDA)
@@ -26,7 +37,8 @@ score_bord = ScoreBord(CH_7SEG_DP, P_7SEG_SCL, P_7SEG_SDA)
 score_bord.set_i2c_addr(ADD_7SEG_D)
 game_logic = ReflexesGame(lights, [InputSwitch(pin_no) for pin_no in P_BUTTONS],
                           Buzzer(P_BUZZER_L), Buzzer(P_BUZZER_H), score_bord,
-                          Led(P_HIGHSCORE), Led(P_GAMEOVER))
+                          Led(P_HIGHSCORE), Led(P_GAMEOVER),
+                          order_size)
 game_logic.parts_check()
 # ------------------------------------------------------------------------------
 
