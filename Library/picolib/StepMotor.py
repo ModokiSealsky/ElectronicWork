3026f4e4e348c530537222bd656cb1149aae99ff
yasutoshikitahara <yasutoshi.kitahara@gmail.com>
2025-07-22 22:36:25 +0900

    ステッピングモーターの抽象親クラスを仕様変更。 回転をステップ数と角度で指定可能に変更。

--- Library/picolib/StepMotor.py
+++ Library/picolib/StepMotor.py
@@ -1,34 +1,123 @@
-from enum import Enum
-
-from machine import Pin
-
-class StepMortorType(Enum):
-    _28BY48 = "28BY48"
-    _NAME17 = "NAME1"
+import time
 
 
 class StepMotor:
-    """ステップモーターのインターフェース"""
-    def setBaseVolPin(self, pin_no):
-        """基準電圧ピン設定
+    """ステッピングモーター制御(抽象)クラス"""
 
-        GPIOを3.3V出力として使用する場合、ピン番号を指定する
+    def __init__(self, is_counter: bool = False, one_lap_step: int = 200):
+        """コンストラクタ
 
         Args:
-            pin_no (int): 基準電圧ピン番号
+            is_counter: 反時計回り(正負逆転)指定
+            one_lap_step: 1回転ステップ数
         """
-        Pin(pin_no, Pin.OUT).value(1)
+        print("init start")
+        self.set_counterclockwise(is_counter)
+        self.set_one_lap_step(one_lap_step)
+        print("init end")
 
-    def step(self, step_count: int):
+    def set_counterclockwise(self, is_counter: bool):
+        """反時計回り(正負逆転)指定
+
+        Args:
+            is_counter: ステップ数がプラス値で反時計回りならTrue
+        """
+        self.__is_counter = is_counter
+        print(f"set_counterclockwise:{self.__is_counter}")
+
+    def set_one_lap_step(self, step: int):
+        """1回転ステップ数指定
+
+        Args:
+            step: 1回転に必要なステップ数
+        """
+        self.__one_lap_step = step
+        self.__angle_racio = 360 / self.__one_lap_step
+        print(
+            f"set_one_lap_step:{self.__one_lap_step} | angle_racio:{self.__angle_racio}"
+        )
+
+    def turn_step(self, step: int):
         """指定ステップ数回転
+
         Args:
-            step_count (int): 回転ステップ数（負数は逆転方向）
+            step: 回転ステップ数（正の値は時計回り）
         """
-        print("step_count:{0}".format(step_count))
+        if self.__is_counter:
+            step = -step
+        self.__turn_step(step)
 
     def turn_angle(self, angle: float):
-        """指定角度回転)
+        """指定角度回転
+
         Args:
-            angle: 回転角度（負数は反時計回り）
+            angle: 回転角度（正の値は時計回り）
         """
-        print("angle:{0}".format(angle))
+        step = int(angle / self.__angle_racio)
+        self.turn_step(step)
+
+    def __turn_step(self, step: int):
+        """指定ステップ数回転（子クラスで実装する）
+
+        Args:
+            step: 回転ステップ数（正の値は時計回り）
+        """
+        print(f"turn_step:{step}")
+
+
+class StepMotorTester:
+    """テスタークラス"""
+
+    def __init__(self, target: StepMotor):
+        self.__target_clz = target
+
+    def turn_test(self):
+        # turn step
+        print("turn_step(100)")
+        self.__target_clz.turn_step(100)
+        time.sleep(1)
+        print("turn_step(-100)")
+        self.__target_clz.turn_step(-100)
+        time.sleep(1)
+        print("set_counterclockwise(True)")
+        self.__target_clz.set_counterclockwise(True)
+        print("turn_step(100)")
+        self.__target_clz.turn_step(100)
+        time.sleep(1)
+        print("turn_step(-100)")
+        self.__target_clz.turn_step(-100)
+        time.sleep(1)
+        self.__target_clz.set_counterclockwise(False)
+        # turn angle
+        print("turn_angle(90)")
+        self.__target_clz.turn_angle(90)
+        time.sleep(1)
+        print("turn_angle(-90)")
+        self.__target_clz.turn_angle(-90)
+        time.sleep(1)
+        self.__target_clz.set_one_lap_step(400)
+        print("turn_angle(90)")
+        self.__target_clz.turn_angle(90)
+        time.sleep(1)
+        print("turn_angle(-90)")
+        self.__target_clz.turn_angle(-90)
+        time.sleep(1)
+        self.__target_clz.set_counterclockwise(True)
+        print("turn_angle(90)")
+        self.__target_clz.turn_angle(90)
+        time.sleep(1)
+        print("turn_angle(-90)")
+        self.__target_clz.turn_angle(-90)
+
+
+# ==================
+# テストコード
+# ==================
+if __name__ == "__main__":
+    print("test start ----")
+    step_motor = StepMotor()
+    tester = StepMotorTester(step_motor)
+    tester.turn_test()
+    print("--------")
+    step_motor = StepMotor(True, 500)
+    print("test end   ----")
