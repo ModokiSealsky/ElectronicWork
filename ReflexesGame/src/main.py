0fd50c75cbe23b044c84af8640ed1a48c100603b
yasutoshikitahara <yasutoshi.kitahara@gmail.com>
2025-07-31 22:41:16 +0900

    メイン処理のライブラリ対応

--- ReflexesGame/src/main.py
+++ ReflexesGame/src/main.py
@@ -1,22 +1,28 @@
 """反応速度を測るゲーム_メイン(待機)ループ"""
+
 import utime
 
 from PicoLib import Buzzer, InputSwitch, Led
-from reflexesgame import HiLowBuzzer, ReflexesGame, ResultLight, ScoreBord, ScoreBordDig4Seg7
+from reflexesgame import (
+    ReflexesGame,
+    ResultLight,
+    ScoreBordDig4Seg7,
+    SoundEffectUnitByTwinBuzzer,
+)
 
 # ピン指定 ---------------------------------------------------------------------
-P_BUZZER_L  = 17
-P_BUZZER_H  = 16
-P_LIGHTS    = [2 ,4 ,6, 8, 10, 12]
-P_BUTTONS   = [3, 5, 7, 9, 11, 13]
-P_MODE_SEL  = 18
+P_BUZZER_L = 17
+P_BUZZER_H = 16
+P_LIGHTS = [2, 4, 6, 8, 10, 12]
+P_BUTTONS = [3, 5, 7, 9, 11, 13]
+P_MODE_SEL = 18
 P_START_BTN = 19
 P_HIGHSCORE = 14
-P_GAMEOVER  = 15
-CH_7SEG_DP  = 0
-ADD_7SEG_D  = 0x70
-P_7SEG_SDA  = 20
-P_7SEG_SCL  = 21
+P_GAMEOVER = 15
+CH_7SEG_DP = 0
+ADD_7SEG_D = 0x70
+P_7SEG_SDA = 20
+P_7SEG_SCL = 21
 # ------------------------------------------------------------------------------
 
 # セットアップ -----------------------------------------------------------------
@@ -34,22 +40,32 @@ lights = [Led(pin_no) for pin_no in P_LIGHTS]
 start_btn = InputSwitch(P_START_BTN)
 score_bord = ScoreBordDig4Seg7(CH_7SEG_DP, P_7SEG_SCL, P_7SEG_SDA)
 score_bord.set_i2c_addr(ADD_7SEG_D)
-game_logic = ReflexesGame(lights, [InputSwitch(pin_no) for pin_no in P_BUTTONS],
-                          HiLowBuzzer(Buzzer(P_BUZZER_H), Buzzer(P_BUZZER_L)), score_bord,
-                          ResultLight(Led(P_HIGHSCORE), Led(P_GAMEOVER)),
-                          order_size)
-game_logic.parts_check()
+score_bord.display_check()
+se_unit = SoundEffectUnitByTwinBuzzer(Buzzer(P_BUZZER_H), Buzzer(P_BUZZER_L))
+se_unit.check()
+game_logic = ReflexesGame(
+    lights,
+    [InputSwitch(pin_no) for pin_no in P_BUTTONS],
+    se_unit,
+    score_bord,
+    ResultLight(Led(P_HIGHSCORE), Led(P_GAMEOVER)),
+    order_size,
+)
 # ------------------------------------------------------------------------------
 
 # 待機時アニメーション -------------------------------------------------------------------
 lights_idx: int = 0
 lights_len: int = len(lights)
+
+
 def waitAnimation(idx: int, _len: int):
     for light in lights:
         light.off()
     idx = (idx + 1) % _len
     lights[idx].on()
     return idx
+
+
 # ------------------------------------------------------------------------------
 
 # 待機ループ -------------------------------------------------------------------
