cfb5cb98d09fced7a49eea4785abf208e14eaf3b
yasutoshikitahara <yasutoshi.kitahara@gmail.com>
2025-07-31 22:24:42 +0900

    issues #6 パッケージ定義修正

--- ReflexesGame/src/reflexesgame/__init__.py
+++ ReflexesGame/src/reflexesgame/__init__.py
@@ -3,3 +3,6 @@ from .ScoreBord import ScoreBord
 from .ScoreBordDig4Seg7 import ScoreBordDig4Seg7
 from .ResultLight import ResultLight
 from .ReflexesGame import ReflexesGame
+from .SoundEffectUnit import SoundEffectUnit
+from .SoundEffectUnitByPwm import SoundEffectUnitByPwm
+from .SoundEffectUnitByTwinBuzzer import SoundEffectUnitByTwin
