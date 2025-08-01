7967cf42948071794a64295c5a2b7ec54c73af9f
yasutoshikitahara <yasutoshi.kitahara@gmail.com>
2025-08-01 22:37:59 +0900

    入力ボタンクラスにPULL_DOWN、PULL_UP切替、入力値の逆転指定を追加 ※pico2初期版にあるGPIO不具合への対応

--- Library/picolib/InputSwitch.py
+++ Library/picolib/InputSwitch.py
@@ -1,44 +1,68 @@
 import utime
 
 from machine import Pin
+from micropython import const
 
 
-class InputSwitch:
+class InputSwitch(object):
     """入力スイッチクラス"""
 
-    def __init__(self, pin_no: int):
+    __swt: Pin
+    """スイッチのピンオブジェクト"""
+    __on_val: int
+    """ボタンON値"""
+
+    def __init__(self, pin_no: int, is_pull_up: bool = False, is_reverse: bool = False):
         """
         Args:
             pin_no: GPIOピン番号
+            is_pull_up: PullUp指定(回路をプルアップ、スイッチの先がGNDで組む場合にTrue)
+            is_reverse: ON/OFF逆転指定
         """
-        self._swt = Pin(pin_no, Pin.IN, Pin.PULL_DOWN)
-        self._is_before_on: bool = False
-        self._on_torriger: bool = False
-        self._off_torriger: bool = False
+        if is_pull_up:
+            self.__swt = Pin(pin_no, Pin.IN, Pin.PULL_UP)
+        else:
+            self.__swt = Pin(pin_no, Pin.IN, Pin.PULL_DOWN)
+        if is_reverse:
+            self.__on_val = const(0)
+        else:
+            self.__on_val = const(1)
 
     def is_on(self):
         """スイッチがONかを確認"""
-        return self._swt.value() == 1
+        return self.__swt.value() == self.__on_val
 
     def is_off(self):
         """スイッチがOFFかを確認"""
-        return self._swt.value() == 0
+        return self.__swt.value() != self.__on_val
+
+    def test(self, test_time: int = 10):
+        """テストコード"""
+        print("test start ----")
+        cnt = 0
+        while cnt < test_time:
+            self.__print_value(cnt)
+            cnt += 1
+            utime.sleep(1)
+        print("test end   ----")
+
+    def __print_value(self, cnt: int):
+        print("btn on:{0}/off:{1}| cnt:{2}".format(self.is_on(), self.is_off(), cnt))
 
 
 # ==================
 # テストコード
 # ==================
 if __name__ == "__main__":
-    print("test start ----")
+    print("test down normal ----")
     cls = InputSwitch(0)
-    keep_on_cnt = 0
-    while keep_on_cnt < 4:
-        print(
-            "btn on:{0}/off:{1}| cnt:{2}".format(cls.is_on(), cls.is_off(), keep_on_cnt)
-        )
-        if cls.is_on():
-            keep_on_cnt += 1
-        else:
-            keep_on_cnt = 0
-        utime.sleep(1)
-    print("test end   ----")
+    cls.test()
+    print("test up normal ----")
+    cls = InputSwitch(0, True)
+    cls.test()
+    print("test down reverse ----")
+    cls = InputSwitch(0, False, True)
+    cls.test()
+    print("test up reverse ----")
+    cls = InputSwitch(0, True, True)
+    cls.test()
